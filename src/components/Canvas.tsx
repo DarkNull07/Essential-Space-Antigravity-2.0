6ef054383dd08bafe3ef91a04057bca0f8a87135
DarkNull07 <darknull07@gmail.com>
2026-07-29 11:32:06 +0530

    update

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1332,13 +1332,7 @@ export default function Canvas({
       )}
 
       {/* Grid Canvas Sortable Section */}
-      <motion.section
-        custom={4}
-        initial="hidden"
-        animate="visible"
-        variants={fadeUpVariant}
-        className="flex-grow"
-      >
+      <section className="flex-grow">
         {filteredCards.length > 0 ? (
           (() => {
             const activeCard = mounted ? cards.find((c) => String(c.id) === activeId) : undefined;
@@ -1430,7 +1424,7 @@ export default function Canvas({
             </div>
           )
         )}
-      </motion.section>
+      </section>
 
       {/* Neo-Brutalist Rename Category Modal */}
       {isRenameModalOpen && activeCategory && (
